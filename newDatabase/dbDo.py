50d16de96c2e9611b158e373fdafd067e648930d
Joe S <kenwood364@gmail.com>
2019-05-07 13:28:54 -0400

    Added a new set of files and data, changes in desc
    Added the ability to detect the existance of databases
    new bugs:
    sql is not defined as a global var

--- newDatabase/dbDo.py
+++ newDatabase/dbDo.py
@@ -3,29 +3,39 @@ import sys
 import os
 from os.path import isfile, getsize
 
-c = conn.cursor()
 
 def initDB(fileInQuestion): #database initDB
 	print("looking for " + fileInQuestion  + ".db") #print to console
 	if os.path.isfile(fileInQuestion + ".db"): #ask if the file exists
 		print("Found File") #it exists
-		conn = sqlite3.connect(fileInQuestion + '.db') #connect to the database
-	else: #it does not exist
-		print("Did not find database, making new...") #alert console
-		c.execute('''CREATE TABLE in (itemName text, condition text, Memo text, setOf real, date text, issuer text, user text)''') #create a table for items in
-		c.execute('''CREATE TABLE out (itemName text, condition text, Memo text, setOf real, date text, issuer text, user text)''') #create a table for items out
 
-def addItem(content)
-	#recive a list
-	#write the list
+		conn = sqlite3.connect(fileInQuestion + '.db') #THIS DOES NOT WORK
+		c = conn.cursor()
+		
+	else: #it does not exist
+	
+		conn = sqlite3.connect(fileInQuestion + '.db') #THIS DOES NOT WORK
+		c = conn.cursor()
+		
+		print("Did not find database, making new...") #alert console
+		c.execute('''CREATE TABLE itemLoggedIn (itemName text, condition text, Memo text, setOf real, date text, issuer text, user text)''') #create a table for items in
+		c.execute('''CREATE TABLE itemLoggedOut (itemName text, condition text, Memo text, setOf real, date text, issuer text, user text)''') #create a table for items out
+
+def addItem(content):
+	c.executemany('INSERT INTO itemLoggedIn VALUES (?,?,?,?,?,?,?)', content) #insert content list into in
 	conn.commit() #commit the change to the database
 
-def deleteItem(content)
+def deleteItem(content):
 	#receive an item name
 	#remove the line
-	conn.commit() conn.commit() #commit the change to the database
+	conn.commit() #commit the change to the database
 	
-def query(content)
+def queryAll(table):
+	c.execute('SELECT * FROM table') #Select everything
+	print(c.fetchall())
+	
+def query(content):
+	print("nothing")
 	#receive a list
 	#select all matching entries
 	#return a much larger list
--- newDatabase/src.py
+++ newDatabase/src.py
@@ -3,3 +3,6 @@ from tkinter import *
 from tkinter import ttk
 
 dbDo.initDB("manifest")
+print("Loading Gui")
+
+dbDo.addItem([("Screwdriver", "Good", "No Memo", 4, "today", "Jeff", "Jhon")])
