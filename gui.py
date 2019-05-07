f64e483cab208b813b3b323e6e67cb8281910db4
J <31870999+KenwoodFox@users.noreply.github.com>
2019-05-06 21:34:53 -0400

    push push

--- gui.py
+++ gui.py
@@ -12,6 +12,7 @@ home = Tk() #Home is an instance of Tk
 home.title("Inventory Management Tool Version 0.1Alpha") #Set title
 conn = sqlite3.connect('database.db') #establish a connection with the database
 c = conn.cursor() #create a cursor object
+#add a check here to make sure its not alreadi init to comit it
 c.execute('''CREATE TABLE items (itemName text, condition text, Memo text, setOf real, date text, issuer text, user text, location text)''')
 
 homeTabs = ttk.Notebook(home) #add tabs to home
@@ -60,13 +61,10 @@ memoTxt.grid(column=6,row=3)
 setOfTxt.grid(column=8,row=3)
 
 def newItem():	#run if the new item button is clicked
-	itemname = itemNameTxt.get()
-	condition = conditionTxt.get()
-	memo = memoTxt.get()
-	setOf = setOfTxt.get()
+	newItemList =[(itemNameTxt.get(), conditionTxt.get(), memoTxt.get(), setOfTxt.get(), dateNow, 'fixthis', 'fixthis', 'in')]
 	
 	#insert data
-	c.execute("INSERT INTO items VALUES (itemName, condition, Memo, setOf, dateNow, 'fixthis', 'fixthis', 'in')")
+	c.executemany('INSERT INTO items VALUES (?,?,?,?,?,?,?,?)', newItemList)
 
 setNewItem = Button(itemTab, text="Add Item", command=newItem)
 setNewItem.grid(column=2, row=1)
