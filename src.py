1a99e50423cfb07f93f590d76d6c1ac2677f6556
Joe S <kenwood364@gmail.com>
2019-05-08 11:11:34 -0400

    Removed debug def

--- src.py
+++ src.py
@@ -5,9 +5,4 @@ from tkinter import ttk
 dbDo.initDB("manifest")
 print("Loading Gui")
 
-def addItem(content, database):
-	c.executemany('INSERT INTO itemLoggedIn VALUES (?,?,?,?,?,?,?)', content) #insert content list into in
-	conn.commit() #commit the change to the database
-
 #dbDo.addItem([("Screwdriver", "Good", "No Memo", 4, "today", "Jeff", "Jhon")], "manifest")
-addItem([("Screwdriver", "Good", "No Memo", 4, "today", "Jeff", "Jhon")], "manifest")
