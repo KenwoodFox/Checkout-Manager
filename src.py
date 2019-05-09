1f6fccc58a76f566ce6717323f8790fe244c42a5
Joe S <kenwood364@gmail.com>
2019-05-09 09:25:35 -0400

    init to comit it

--- src.py
+++ src.py
@@ -1,6 +1,25 @@
-import dbDo as dbDo
-from tkinter import *
-from tkinter import ttk
+#test.py
+try:
+    import tkinter as tk  # for python 3
+except:
+    import Tkinter as tk  # for python 2
+import pygubu
 
 
-#dbDo.addItem([("Screwdriver", "Good", "No Memo", 4, "today", "Jeff", "Jhon")], "manifest")
+class Application:
+    def __init__(self, master):
+
+        #1: Create a builder
+        self.builder = builder = pygubu.Builder()
+
+        #2: Load an ui file
+        builder.add_from_file('gui.ui')
+
+        #3: Create the widget using a master as parent
+        self.mainwindow = builder.get_object('parentWidget', master)
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    app = Application(root)
+    root.mainloop()
